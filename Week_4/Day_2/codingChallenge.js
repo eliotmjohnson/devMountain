65162ae55a4ed1ca05a9a03173456c7c6ac7da38
Eliot Johnson <eliotmjohnson@gmail.com>
2023-04-11 09:46:59 -0500

    Init Commit

---
+++ Week_4/Day_2/codingChallenge.js
@@ -0,0 +1,21 @@
+const makeUnique = (string) => {
+	let newSet = new Set(string);
+	let newString = [];
+	newSet.forEach((e) => newString.push(e));
+	return newString.join("");
+};
+
+const makeUnique = (string) => {
+	let arr = string.split("");
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] === arr[i]) {
+				arr.splice(j, 1);
+			}
+		}
+	}
+	return arr.join("");
+};
+
+console.log(makeUnique("helloworld"));
+console.log(makeUnique("iwanttoclimbamountain"));
